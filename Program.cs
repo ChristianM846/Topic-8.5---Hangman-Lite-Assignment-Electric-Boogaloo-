941114adf4c431caf3ea18a78d77d89e6b473cfd
Christian Moyes <339463846@GoTVDSB.ca>
2024-09-26 09:11:30 -0400

    Difficulty

--- Program.cs
+++ Program.cs
@@ -5,24 +5,52 @@
         static void Main(string[] args)
         {
             string word, displayWord, guess, response;
-            int incorrect, tempLength;
+            int incorrect, tempLength, difficulty;
             bool match, done;
             match = false;
             done = false;
+            word = "";
             displayWord = "";
             incorrect = 0;
             List<string> lettersGuessed = new List<string>();
-            List<string> wordList = new List<string>() { "COMPUTER", "PANCAKES", "SMILE", "ENTERTAINMENT", "AVATAR", "GAMING" };
+            List<string> easyWordList = new List<string>() { "EASY" };
+            List<string> medWordList = new List<string>() { "MEDIUM" };
+            List<string> hardWordList = new List<string>() { "COMPUTER", "PANCAKES", "SMILE", "ENTERTAINMENT", "AVATAR", "GAMING" };
             Random generator = new Random();
 
-            Console.WriteLine("I want to play a game");
-            Console.WriteLine("A game of hangman to be exact");
+            Console.WriteLine("I want to play a game.");
+            Console.WriteLine("A game of hangman to be exact!");
+            Console.WriteLine();
             Console.WriteLine($"You know the rules: There is a secret word, and you must guess one letter at a time to figure it out");
-            Console.WriteLine("Unlike normal hangman, you only get three wrong guesses before you lose. Let's begin");
+            Console.WriteLine("Unlike normal hangman, you only get three wrong guesses before you lose. Let's begin!");
+            Console.WriteLine();
 
             while (!done)
             {
-                word = wordList[generator.Next(0, 6)];
+                Console.WriteLine("What difficulty would you like to play?");
+                Console.WriteLine();
+                Console.WriteLine("1 - Easy (3-5 letters)");
+                Console.WriteLine("2 - Medium (6-8 letters)");
+                Console.WriteLine("3 - Hard (9+ letters)");
+
+                while (!Int32.TryParse(Console.ReadLine().Trim(), out difficulty) || difficulty <= 0 || difficulty > 3)
+                {
+                    Console.WriteLine("Please input the number matching your desired difficulty. ");
+                }
+
+                if (difficulty == 1)
+                {
+                    word = easyWordList[generator.Next(0, easyWordList.Count)];
+                }
+                else if (difficulty == 2)
+                {
+                    word = medWordList[generator.Next(0, medWordList.Count)];
+                }
+                else if (difficulty == 3)
+                {
+                    word = hardWordList[generator.Next(0, hardWordList.Count)];
+                }
+
                 tempLength = word.Length;
                 displayWord = "";
 
@@ -39,7 +67,7 @@
                 incorrect = 0;
                 match = false;
 
-                while (incorrect < 3 && match == false)
+                while (incorrect < 5 && match == false)
                 {
                     if (incorrect == 0)
                     {
@@ -53,6 +81,18 @@
                     {
                         TwoIncorrect();
                     }
+                    else if (incorrect == 3)
+                    {
+                        ThreeIncorrect();
+                    }
+                    else if (incorrect == 4)
+                    {
+                        FourIncorrect();
+                    }
+                    else if (incorrect == 5)
+                    {
+                        FiveIncorrect();
+                    }
 
                     Console.WriteLine($"Current known letters: {displayWord}");
                     Console.WriteLine();
@@ -64,7 +104,7 @@
                     }
 
                     Console.WriteLine();
-                    Console.WriteLine($"You have {3 - incorrect} incorrect guesses left");
+                    Console.WriteLine($"You have {5 - incorrect} incorrect guesses left");
                     Console.WriteLine();
                     Console.WriteLine("What letter would you like to guess:");
                     guess = Console.ReadLine().ToUpper().Trim();
@@ -72,7 +112,7 @@
 
                     while (guess == "" || guess.Length > 1 || lettersGuessed.Contains(guess))
                     {
-                        Console.WriteLine("I'm sorry, please restrict your guesses to one character at a time and esure you haven't already guessed it");
+                        Console.WriteLine("I'm sorry, please restrict your guesses to one character at a time and ensure you haven't already guessed it");
                         guess = Console.ReadLine().ToUpper().Trim();
                         Console.Beep();
                     }
@@ -122,27 +162,26 @@
                     Console.WriteLine($"Too bad, you weren't able to find the word. The word this time around was {word}");
                 }
 
-                Console.WriteLine("Would you like to play again? (Yes or No)");
-                response = Console.ReadLine().ToUpper().Trim();
+                Console.WriteLine("Would you like to play again? (y/n)");
+                response = Console.ReadLine().ToLower().Trim();
 
-                while (response != "YES" && response != "NO")
+                while (response != "y" && response != "n")
                 {
                     Console.WriteLine("That's not a valid response. Yes or No please.");
                     response = Console.ReadLine().ToUpper().Trim();
                 }
 
-                if (response == "YES")
+                if (response == "y")
                 {
                     Console.WriteLine("Okay, let's do it! Press ENTER to continue.");
                     Console.ReadLine();
                     Console.Clear();
                 }
-                else if (response == "NO")
+                else if (response == "n")
                 {
                     Console.WriteLine("Okay, I hope you enjoyed playing. Goodbye!");
                     done = true;
                 }
-
             }
         }
 
@@ -173,13 +212,35 @@
             Console.WriteLine("  +---+");
             Console.WriteLine("  |   |");
             Console.WriteLine("  O   |");
-            Console.WriteLine(" /|\\  |");
+            Console.WriteLine(" /|   |");
             Console.WriteLine("      |");
             Console.WriteLine("      |");
             Console.WriteLine("=========");
         }
 
         public static void ThreeIncorrect()
+        {
+            Console.WriteLine("  +---+");
+            Console.WriteLine("  |   |");
+            Console.WriteLine("  O   |");
+            Console.WriteLine(" /|\\  |");
+            Console.WriteLine("      |");
+            Console.WriteLine("      |");
+            Console.WriteLine("=========");
+        }
+
+        public static void FourIncorrect()
+        {
+            Console.WriteLine("  +---+");
+            Console.WriteLine("  |   |");
+            Console.WriteLine("  O   |");
+            Console.WriteLine(" /|\\  |");
+            Console.WriteLine(" /    |");
+            Console.WriteLine("      |");
+            Console.WriteLine("=========");
+        }
+
+        public static void FiveIncorrect()
         {
             Console.WriteLine("  +---+");
             Console.WriteLine("  |   |");
